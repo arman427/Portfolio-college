f8e89d1cb792aff960a23065fa82a3b9eb204de3
Arman <babayananuta11@gmail.com>
2025-10-02 21:23:03 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+// --------------- Анимация при скролле ---------------
+const elements = document.querySelectorAll('.anim-item');
+
+function scrollAnim() {
+   let scroll = window.scrollY + window.innerHeight;
+   elements.forEach(element => {
+      const elementHeight = element.offsetTop + element.offsetHeight * 0.4;
+      if (scroll > elementHeight) {
+         element.classList.add('active')
+      }
+   })
+}
+window.addEventListener('scroll', scrollAnim);
+scrollAnim();
+// --------------- Анимация при скролле ---------------
+
+
+
+
+
+// --------------- Бургер меню ---------------
+const navIconBtn = document.querySelector('.nav-icon-btn');
+const navIcon = document.querySelector('.nav-icon');
+const mobileNav = document.querySelector('.header-content');
+const html = document.querySelector('html')
+const burgerLinks = document.querySelectorAll('.burger-link')
+
+navIcon.addEventListener('click', () => {
+   navIcon.classList.toggle('nav-icon--active');
+   mobileNav.classList.toggle('header-content__mobile');
+   html.classList.toggle('no-scroll');
+});
+
+burgerLinks.forEach(index => {
+   index.addEventListener('click', () => {
+      mobileNav.classList.remove('header-content__mobile');
+      html.classList.remove('no-scroll');
+   })
+});
+// --------------- Бургер меню ---------------
